495ac3f23aa40486fc61e29fa3aadad4ad3d530f
priscigabe <132255304+priscigabe@users.noreply.github.com>
2023-07-25 14:28:40 +0100

    Create README.

---
+++ README.m
@@ -0,0 +1,10 @@
+- 👋 I’m @priscigabe
+- 👀 I’m interested in optometry, crypto, forex 
+- 🌱 I’m currently learning french
+- 💞️ I’m looking to collaborate  all
+- 📫 How to reach me +2348084226895
+
+<!---
+priscigabe/priscigabe is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
